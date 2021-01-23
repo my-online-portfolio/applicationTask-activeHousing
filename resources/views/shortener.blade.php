dcf2e413133929cd3ac81d5f83873bc47fca6417
Jay James <design@themadlab.uk>
2021-01-23 17:50:30 +0000

    Added container, row and col

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -1,1 +1,21 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col">Row 1 Col 1</div>
+            </div>
+            <div class="row">
+                <div class="col">Row 2 Col 1</div>
+            </div>
+        </div>
+    </body>
+</html>
