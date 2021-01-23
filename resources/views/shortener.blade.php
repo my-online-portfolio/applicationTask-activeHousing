751e64e4197899d559253dddd17c91b71069f0e4
Jay James <design@themadlab.uk>
2021-01-23 18:06:56 +0000

    Added stylesheet reference

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -6,7 +6,7 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="container-fluid">
