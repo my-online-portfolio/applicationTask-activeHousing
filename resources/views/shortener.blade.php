cf53b18325d6c23e1c9b82c57994ec70c0c78346
Jay James <design@themadlab.uk>
2021-01-23 20:10:35 +0000

    Added random elements for testing. Removed obsolete ones.

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -6,18 +6,23 @@
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">-->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row h-80vh-min">
-                <div class="col">Row 1 Col 1</div>
-            </div>
-            <div class="row h-20vh-min">
-                <div class="col">Row 2 Col 1</div>
-            </div>
-        </div>
+        <h1>This is a h1</h1>
+        <br>
+        <a href="http://bbc.co.uk">This is a visited link</a>
+        <br>
+        <a href="http://bbc.co.uk2" target="_blank">This is an unvisited link</a>
+        <br>
+        <button type="submit">This is Submit button</button>
+        <br>
+        <button type="reset">This is Reset button</button>
+        <br>
+        <button type="button">This is a button</button>
+        <br>
+        <input type="button" value="This is also a button">
         <script src="{{ asset('js/jquery-3.4.1.slim.min.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min') }}"></script>
