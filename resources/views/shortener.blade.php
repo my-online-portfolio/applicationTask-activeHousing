e08dd62f61245f711815bffadf12c94e4d867bff
Jay James <design@themadlab.uk>
2021-01-24 19:41:14 +0000

    All fake data complete

--- resources/views/shortener.blade.php
+++ resources/views/shortener.blade.php
@@ -10,68 +10,49 @@
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
-        <!-- HEADER -->
-        <header class="h-30vh-min">
-            <section class="mt-0">
-                <img class="logo mt-40" src="{{ asset('img/hn-bit-logo.png') }}">
-            </section>
-        </header>
-        <!-- MAIN -->
-        <main class="h-70vh-min">
-            <section class="hide">
-                <div id="lipsum">
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tincidunt, massa vel laoreet venenatis, nunc sapien congue mauris, a pharetra odio ligula sit amet velit. Nullam et purus in enim tristique congue et dignissim tellus. Quisque ullamcorper rutrum posuere. Duis vehicula nibh id tristique ultricies. Aliquam egestas malesuada sem, ac gravida metus dapibus vel. Nam in auctor lectus, a scelerisque sem. Phasellus fermentum, eros dictum laoreet placerat, nibh dui finibus libero, eu ullamcorper est ex sit amet libero. Donec at elementum ex. Proin gravida fringilla enim. Integer eleifend a enim placerat mollis. Morbi ullamcorper pretium justo, in placerat metus.
-                    </p>
-                    <p>
-                    Quisque a lobortis ipsum. Nullam gravida enim ut blandit ullamcorper. Aenean fringilla ipsum ac justo fringilla facilisis. Suspendisse potenti. Fusce fringilla, tellus in laoreet sollicitudin, metus risus rhoncus odio, at vehicula elit urna non quam. Nullam pulvinar, risus quis facilisis pretium, sapien magna maximus tellus, fringilla commodo metus velit ac ex. Sed risus tellus, facilisis a lacinia quis, tincidunt vitae sapien.
-                    </p>
-                    <p>
-                    In hac habitasse platea dictumst. Curabitur pulvinar, lorem non volutpat efficitur, velit mi mattis turpis, sit amet ullamcorper purus nisl id ante. Praesent fermentum libero ut nisl gravida dictum. Vestibulum eget ullamcorper purus, ac euismod ex. Nam ac ullamcorper ex. Cras dignissim semper orci vitae tincidunt. Mauris a fringilla eros, ornare cursus mi.
-                    </p>
-                    <p>
-                    Aliquam rutrum enim at ornare vestibulum. Duis scelerisque porttitor turpis, et commodo ante porta laoreet. Maecenas eget sapien dolor. Sed congue lacinia nisi, id laoreet justo laoreet eu. Vivamus rhoncus neque venenatis justo laoreet iaculis. Duis fringilla nunc et leo malesuada, at euismod arcu ullamcorper. Proin semper metus neque, vel ultricies turpis efficitur in. Nunc elementum lorem sit amet orci sollicitudin, et mattis ligula tincidunt. Nullam ut diam id quam aliquam tempus sed sit amet purus. Ut nec volutpat enim. Morbi id lorem at diam dignissim tempus at vel odio. Etiam nec dictum dui. Maecenas nec mi vel purus ornare venenatis vitae sed tortor. Pellentesque magna orci, cursus nec elementum quis, commodo eu turpis. Maecenas vel nisi in ex maximus pulvinar.
-                    </p>
-                    <p>
-                    Mauris odio massa, laoreet et tincidunt id, ultrices et metus. Integer laoreet nec tellus vitae euismod. Maecenas dignissim aliquam erat id tincidunt. In velit ligula, laoreet id placerat vel, condimentum at erat. Curabitur bibendum velit in eros pellentesque, at faucibus nunc sollicitudin. Vivamus tristique sem erat, at iaculis elit facilisis ut. Donec varius sem at arcu hendrerit, sit amet dignissim odio euismod. Vivamus vestibulum turpis sollicitudin dui molestie porttitor. Sed vulputate est at turpis pellentesque tempus ut eu lacus. Fusce vitae nulla sit amet dui ullamcorper luctus eget non nibh. Suspendisse potenti. Praesent lacinia bibendum sapien. Suspendisse lacinia leo vitae suscipit feugiat. Aliquam suscipit eu risus commodo ullamcorper. Phasellus euismod nibh massa, sit amet tempus turpis convallis et.
-                    </p>
-                </div>
-            </section>
-            <section class="mr-40">
-                <div>
-                    <form>
-                        <table>
-                            <tr>
-                                <td>
-                                    <input type="url" id="urlInput" name="urlInput" value="" placeholder="http://yoururl.here">
-                                </td>
-                                <td>
-                                    <input type="url" id="urlInput" name="urlInput" value="" placeholder="http://yoururl.here">
-                                </td>
-                                <td>
-                                    <input type="checkbox"><label class="ml-10">Private?</label>
-                                </td>
-                                <td>
-                                    <button type="submit" class="pt-2 pr-25 pb-2 pl-25">Shorten</button>
-                                </td>
-                            </tr>
-                        </table>
-                    </form>
-                </div>
-            </section>
-            <section>
-                <h1>Recent Links</h1>
-                <div class="container">
-                    @for ($i = 1; $i >= 5; $i++)
-                        <div class="row">
-                            <div class="link-short"><a href="http://localhost/ROW{{ $i }}">http://localhost/ROW{{ $i }}</a></div>
-                            <div class="">{{$i}} week(s) apache_get_modules</div>
-                            <div class="link-long"><a href="http://localhost/somemassivelonglinkthatneededshorteningasitwastoolong{{$i}}">http://localhost/somemassivelonglinkthatneededshorteningasitwastoolong{{$i}}</a></div>
-                        </div>
-                    @endfor
-                </div>
-            </section>
-        </main>
+            <!-- HEADER -->
+            <header class="h-30pc pt-40 pb-40">
+                <section class="mt-0">
+                    <img class="logo" src="{{ asset('img/hn-bit-logo.png') }}">
+                </section>
+            </header>
+            <!-- MAIN -->
+            <main class="h-70pc pb-6pc">
+                <section class="mr-40">
+                    <div>
+                        <form>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <input type="url" id="urlInput" name="urlInput" value="" placeholder="http://yoururl.here">
+                                    </td>
+                                    <td>
+                                        <input type="url" id="urlInput" name="urlInput" value="" placeholder="http://yoururl.here">
+                                    </td>
+                                    <td>
+                                        <input type="checkbox"><label class="ml-10">Private?</label>
+                                    </td>
+                                    <td>
+                                        <button type="submit" class="pt-2 pr-25 pb-2 pl-25">Shorten</button>
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </section>
+                <section class="mr-95">
+                    <h1>Recent Links</h1>
+                    <div class="container">
+                        @for ($i = 1; $i <= 5; $i++)
+                            <div class="row px-10">
+                                <div class="link-short"><a href="http://localhost/ROW{{ $i }}">http://localhost/ROW{{ $i }}</a></div>
+                                <div class="mt-10">{{$i}} week(s) apache_get_modules</div>
+                                <div class="link-long mb-10">http://localhost/somemassivelonglinkthatneededshorteningasitwastoolong{{$i}}</div>
+                            </div>
+                        @endfor
+                    </div>
+                </section>
+            </main>
         <script src="{{ asset('js/jquery-3.4.1.slim.min.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min') }}"></script>
