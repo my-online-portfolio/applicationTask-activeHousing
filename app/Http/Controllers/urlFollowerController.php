27809b0154d7b940eb3ff348c0188ba89d382573
Jay James <design@themadlab.uk>
2021-01-25 22:06:36 +0000

    Added vars to table row. Added redirect header. Added increment.

--- app/Http/Controllers/urlFollowerController.php
+++ app/Http/Controllers/urlFollowerController.php
@@ -11,14 +11,25 @@ class urlFollowerController extends Controller
 
     public function redirect($endpoint){
         //lookup the DB and get the information for this endpoint
-        $endpointInformation = DB::table('urlshorteners')->where('generated_url', '=', $endpoint)->limit(1)->get();//database call
-        $endpointInformation = json_decode($endpointInformation, true);
+        $endpointInformation = DB::table('urlshorteners')
+                                ->where('generated_url', '=', $endpoint)
+                                ->limit(1)
+                                ->get();//database call
+        $endpointInformation = json_decode($endpointInformation, true)[0];
 
-        $redirectURL = $endpointInformation[0]['user_url'];
+        $endpointUUID = $endpointInformation['uuid'];
+        $endpointRedirectURL = $endpointInformation['generated_url'];
+        $endpointUserURL = $endpointInformation['user_url'];
+        $endpointDescription = $endpointInformation['description'];
+        $endpointCounter = $endpointInformation['counter'];
+        $endpointDateAdded = $endpointInformation['date_added'];
+        $endpointDateUpdated = $endpointInformation['date_updated'];
 
-        header('Content-Type: text/plain');
+        //update the counter before issuing a redirect
+        $endpointUpdate = DB::table('urlshorteners')
+                            ->where('uuid', $endpointUUID)
+                             ->increment('counter');
         header('Location: '.$redirectURL);
-        print_r($endpointInformation[0]['user_url']);
         exit;
     }
 }
