714a4f5f24b631501c6fbee596278d9a26731a83
Jay James <design@themadlab.uk>
2021-01-25 21:58:22 +0000

    Added lookup and redirect

--- app/Http/Controllers/urlFollowerController.php
+++ app/Http/Controllers/urlFollowerController.php
@@ -9,7 +9,16 @@ class urlFollowerController extends Controller
 {
     //
 
-    public function redirect($customUrl){
-        die($customUrl);
+    public function redirect($endpoint){
+        //lookup the DB and get the information for this endpoint
+        $endpointInformation = DB::table('urlshorteners')->where('generated_url', '=', $endpoint)->limit(1)->get();//database call
+        $endpointInformation = json_decode($endpointInformation, true);
+
+        $redirectURL = $endpointInformation[0]['user_url'];
+
+        header('Content-Type: text/plain');
+        header('Location: '.$redirectURL);
+        print_r($endpointInformation[0]['user_url']);
+        exit;
     }
 }
