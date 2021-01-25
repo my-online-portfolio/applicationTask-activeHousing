7c8cce28400e45752b74fb27910d16655494d7d2
Jay James <design@themadlab.uk>
2021-01-25 15:01:56 +0000

    Added functions to return various elements on request

--- app/Http/Controllers/UrlshortenerController.php
+++ app/Http/Controllers/UrlshortenerController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class UrlshortenerController extends Controller
 {
-    //
+
+    /**
+     * @Function: Fetches data from the recentTen view
+     */
+    private function getRecent10(){
+        $recentList = DB::table('recentten')->get();
+        return $recentList;
+    }
+    
+    public function index()
+    {
+        //render the main page with fields
+        return view('shortener', ['recentList'=>$this->getRecent10()]);
+    }
+    
+    public function create()
+    {
+        //render the main page with fields
+        return view('shortener', ['recentList'=>$this->getRecent10()]);
+    }
 }
