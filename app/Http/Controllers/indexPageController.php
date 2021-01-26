c51a52d9fb43d6a2b0194a2162ceb1646f6f1e6a
Jay James <design@themadlab.uk>
2021-01-26 13:35:01 +0000

    Added get recent 10 func. Added index func.

--- app/Http/Controllers/indexPageController.php
+++ app/Http/Controllers/indexPageController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class indexPageController extends Controller
 {
-    //
+    //get the most recent 10 entries from the database
+    private function getRecent10(){
+        return DB::table('recentten')->get();//database call and return
+    }
+    
+    public function index(){
+        //render the main page with fields
+        return view('shortener', ['recentList'=>$this->getRecent10()]);
+    }
 }
