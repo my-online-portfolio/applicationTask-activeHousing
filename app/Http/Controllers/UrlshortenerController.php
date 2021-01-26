1386a85baaa4133056c81f6a64571e97eb3e28f8
Jay James <design@themadlab.uk>
2021-01-26 14:11:00 +0000

    Added comments higlighting what I want to do. Created effWords var. Created usedWords var. Created getEffWords Func. Created getUsedWords Func. Created makeEndpoint Func. Created checkEndpoint Func.

--- app/Http/Controllers/UrlshortenerController.php
+++ app/Http/Controllers/UrlshortenerController.php
@@ -15,4 +15,24 @@ class UrlshortenerController extends Controller
         //remove when finished writing
         die('EOF');        
     }
+
+
+    //fetch effwords
+    //fetch usedwords
+    //generate endpoint
+    //check new endpoint
+    //--IF endpoint is already used. Regenerate
+    //--ELSE save the endpoint and return
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    private $effWords;
+    private $usedWords;
+
+    private function getEffWords(){}
+    private function getUsedWords(){}
+    private function makeEndpoint(){}
+    private function checkEndpoint(){}
+
 }
